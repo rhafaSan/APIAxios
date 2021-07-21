5b82cfc6434c6bde96995b777a1b927080e12f20
rhafaSan <rhafa99ss@gmail.com>
2021-07-20 22:22:18 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Users></Users>
+    <Todos></Todos>
+    <Photos></Photos>
+    <Comments></Comments>
+    <Posts></Posts>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users.vue';
+import Todos from './components/Todos.vue';
+import Photos from './components/Photos.vue';
+import Comments from './components/Comments.vue';
+import Posts from './components/Posts.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+   name: "App",
+   components:{
+    Users,
+    Todos,
+    Photos,
+    Comments,
+    Posts
+   }
 }
 </script>
 
@@ -23,6 +34,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
+
+#app button {
+  width: 120px;
+  height: 30px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+li {
+  background: #e8e9e8;
+  margin: 10px 350px 10px 350px;
+  border-radius: 8px;
+}
+li img {
+  width: 150px;
+  height: 150px;
+}
+ h2 {
+  
+   margin: 100px 0 80px 0;
+   font-size: 30px;
+ }
+
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+      <ul>
+          <div v-for="post in posts" :key="post.id">
+              <div v-if="post.id%2 != '0' ">
+                <li>
+                    <p><strong>Id: </strong> {{ post.id }}</p>
+                    <p><strong>UserId: </strong> {{ post.userId }}</p>
+                    <p><strong>Title: </strong> {{ post.title }}</p>
+                    <p><strong>Body: </strong> {{ post.body}}</p>
+                    </li>
+             </div>
+          </div>
+      </ul>
+      <button @click="triggerFetch">Posts</button>
+ </div>
+</template>
+
+<script>
+export default 
+{ 
+    name: "Posts",
+    data() {
+        return {
+            posts: [],
+            users: [],
+            comments: [],
+            postURI: "https://jsonplaceholder.typicode.com/posts",
+            userURI: "https://jsonplaceholder.typicode.com/users",
+            commentsURI: "https://jsonplaceholder.typicode.com/comments" 
+            };
+        },
+        methods: {
+            fetchPosts: function() {
+                this.$http.get(this.postURI).then((result) => {
+                    this.posts = result.data;    
+                });
+            },
+            fetchUser: function(){
+                this.$http.get(this.userURI).then((result) => {
+                    console.log(result.data);
+                })
+            },
+            fetchComments: function(){
+                this.$http.get(this.commentsURI).then((result) => {
+                    console.log(result.data);
+                })
+            },
+            triggerFetch: function(){
+                this.fetchPosts()
+                this.fetchUser()
+                this.fetchComments()
+                
+            }
+        },
+};
+</script>
+
+<style>
+
+</style>
